2166e83817588031f31dfcae48f29a7bcba22f9c
José Duque <duquejo01@gmail.com>
2024-02-13 12:24:08 -0500

    :zap: PDF feature assets optimization

--- src/composables/useGeneratePdf.ts
+++ src/composables/useGeneratePdf.ts
@@ -1,18 +1,26 @@
-import { generate } from '@pdfme/generator';
-import { generateInputs, generateBaseTemplate } from '@/components/pdf/config';
-import { text } from '@pdfme/schemas';
-import type { Ref } from 'vue';
-import type { MessageSchema } from '@/i18n/config';
 import { computed } from 'vue';
+import type { Ref } from 'vue';
+import type { GenerateProps } from '@pdfme/common';
+import type { MessageSchema } from '@/i18n/config';
+import { generateInputs, generateBaseTemplate } from '@/components/pdf/config';
+
+const loadPdfGenerator = () => import('@pdfme/generator');
 
 const useGeneratePdf = (locale: Ref<string>, languageSources: Ref<MessageSchema>) => {
 
-  const plugins = { text };
   const template = computed(() => generateBaseTemplate(locale));
   const inputs = computed(() => generateInputs(languageSources));
 
-  const createPdf = () => {
-    generate({ template: template.value, plugins, inputs: inputs.value }).then((pdf) => {
+  const createPdf = async () => {
+
+    const config: GenerateProps = {
+      inputs: inputs.value,
+      template: await template.value,
+    };
+
+    const { generate } = await loadPdfGenerator();
+
+    generate(config).then((pdf) => {
       // Browser
       const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
       window.open(URL.createObjectURL(blob));
